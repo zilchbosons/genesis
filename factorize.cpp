1237b7e6b59f00ce5c8c10f28023d841145fffb1
Subhendra Basu <subhendra.basu@gmail.com>
2016-09-15 01:51:34 +0530

    CodE

--- factorize.cpp
+++ factorize.cpp
@@ -23,75 +23,9 @@
 #include <string>
 #include <gmp.h>
 #include <vector>
-#include <pthread.h>
+#include "common.hpp"
 
 using namespace std;
-
-void factorizeByTrialDivision(void* _nn) {
-	char* nn = (char*) _nn;
-	mpz_t nt;
-	mpz_init(nt);
-	mpz_set_str(nt, nn, 10);
-	mpz_t ct;
-	mpz_init(ct);
-	mpz_set_si(ct, 0);
-	mpz_t st;
-	mpz_init(st);
-	mpz_sqrt(st, nt);
-	mpz_set(ct, st);
-	mpz_t rt;
-	mpz_init(rt);
-	while ( mpz_cmp_si(ct, 1) > 0) {
-		mpz_mod(rt, nt, ct);
-		if (mpz_cmp_si(rt, 0)==0) {
-			cout << "\nFactor1:\t"<< strdup(mpz_get_str(0,10,ct))<<"\n";
-			exit(0);
-		}
-		mpz_sub_ui(ct, ct, 1);
-	}
-	return;
-}
-
-void factorizeByTrialMultiplication(void* _nn)  {
-	char* nn = (char*) _nn;
-	mpz_t nt;
-	mpz_init(nt);
-	mpz_set_str(nt, nn, 10);
-	mpz_t st;
-	mpz_init(st);
-	mpz_mul(st, nt, nt);
-	mpz_t ct;
-	mpz_init(ct);
-	mpz_set(ct, nt);
-	mpz_add_ui(ct,ct, 1);
-	mpz_t rt;
-	mpz_init(rt);
-	mpz_t sqt;
-	mpz_init(sqt);
-	mpz_t term;
-	mpz_init(term);
-	mpz_t term2;
-	mpz_init(term2);
-	while (1) {
-		mpz_mul(term, ct,ct);
-		mpz_sub(rt, term, st);
-		mpz_sqrt(sqt, rt);
-		mpz_set(term2, sqt);
-		mpz_mul(sqt, sqt, sqt);
-		mpz_sub(sqt, rt, sqt);
-		mpz_abs(sqt, sqt);
-		if (mpz_cmp_si(sqt, 0)==0) {
-			mpz_sub(ct, ct, nt);
-			mpz_sub(term2, term2, nt);
-			gmp_printf("\n%Zd\t%Zd\n", ct, term2);
-			mpz_add(ct, ct, term2);
-			cout << "\nFactor2:\t"<< strdup((char*) mpz_get_str(0,10, ct))<<"\n";
-			exit(0);
-		}
-		mpz_add_ui(ct,ct, 1);
-	}
-	return;
-}
 
 
 int main() {
@@ -108,11 +42,6 @@ int main() {
 
 	int l = strlen(nn);
 
-	pthread_t thread1, thread2;
-	pthread_create(&thread1, NULL, (void*(*)(void*) ) &factorizeByTrialDivision,  nn);
-	pthread_create(&thread2, NULL, (void*(*)(void*) ) &factorizeByTrialMultiplication, nn);
-	pthread_join(thread1, NULL);
-	pthread_join(thread2, NULL);
 	fclose(fp);
 	free(nn);
 	return 0;
